4b0b0338071b9912767d44c6efd570a600d69ede
UtkijGadenoK <lucy.andreieva@gmail.com>
2024-07-09 19:02:24 +0700

    fix: avoiding of too many 'returns'

--- src/games/prime.js
+++ src/games/prime.js
@@ -1,14 +1,22 @@
 import { generateRandomNumber } from '../index.js';
 
-const isPrime = (num) => {
-  if (num <= 1) return false;
-  if (num === 2) return true;
-  if (num % 2 === 0) return false;
-  for (let i = 3; i <= Math.sqrt(num); i += 2) {
-    if (num % i === 0) return false;
+function isPrime(num) {
+  if (num <= 1) {
+    return false;
   }
-  return true;
-};
+  if (num === 2) {
+    return true;
+  }
+
+  let prime = true;
+  for (let i = 2; i < num; i += 1) {
+    if (num % i === 0) {
+      prime = false;
+      break;
+    }
+  }
+  return prime;
+}
 
 const generateRound = () => {
   const number = generateRandomNumber();
