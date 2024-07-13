e65635d2ede5b5cdad587a0d5f14b7ce498ec049
UtkijGadenoK <lucy.andreieva@gmail.com>
2024-07-13 14:21:34 +0700

    feat: created function isEven

--- src/games/even.js
+++ src/games/even.js
@@ -1,9 +1,11 @@
 import { runGame } from '../index.js';
 import generateRandomNumber from '../utils.js';
 
+const isEven = (number) => number % 2 === 0;
+
 const generateRound = () => {
   const question = generateRandomNumber(1, 100);
-  const correctAnswer = question % 2 === 0 ? 'yes' : 'no';
+  const correctAnswer = isEven(question) ? 'yes' : 'no';
   return { question, correctAnswer };
 };
 
